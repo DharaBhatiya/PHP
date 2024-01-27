bdb5b6e405013d70dab86170f9af20d9b22de83c
DharaBhatiya <122624543+DharaBhatiya@users.noreply.github.com>
2024-01-27 22:39:47 +0530

    Add files via upload

---
+++ 02_more.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: boreder-box;
+    }
+    .container{
+        max-width:80%;
+        background-color: gray;
+        margin: auto;
+        padding: 30px
+    }
+</style>
+<body>
+   <div class="container">
+    <h1> Lets Learn About PHP....!</h1>
+    This is the container.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae assumenda cupiditate maiores id deserunt nisi dolorum nesciunt perspiciatis quis optio amet numquam ducimus accusantium ut vero quae, expedita aliquam est.
+    <?php
+
+    ?>
+   </div> 
+</body>
+</html>
